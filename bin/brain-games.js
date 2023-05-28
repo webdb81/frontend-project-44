8b25a5379ea53f36f653ba02eec61c55a252c3a7
webdb81 <webdb81@gmail.com>
2023-05-28 11:20:18 +0300

    Step 3: Ask user name and greeting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
+import askUserName from "../src/cli.js";
 
 console.log('Welcome to the Brain Games!');
+
+const userName = askUserName();
+
+console.log(`Hello, ${userName}!`);
---
+++ src/cli.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+
+const askUserName = () => readlineSync.question('May I have your name? ');
+
+export default askUserName;
